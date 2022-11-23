200c32b2c8857560dd244598d9d6d82c0382ce3f
Hyune-c <choi8608@gmail.com>
2022-11-23 15:00:33 +0900

    🔧 commons-csv dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,9 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("org.apache.commons:commons-csv:1.9.0")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
