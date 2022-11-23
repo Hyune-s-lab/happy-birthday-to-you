6ac70ca58b541405538159e0c64c73c23f245d0b
Hyune-c <choi8608@gmail.com>
2022-11-23 14:43:38 +0900

    🔧 project init

---
+++ src/main/kotlin/com/hyunec/happybirthdaytoyou/HappyBirthdayToYouApplication.kt
@@ -0,0 +1,11 @@
+package com.hyunec.happybirthdaytoyou
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HappyBirthdayToYouApplication
+
+fun main(args: Array<String>) {
+    runApplication<HappyBirthdayToYouApplication>(*args)
+}
