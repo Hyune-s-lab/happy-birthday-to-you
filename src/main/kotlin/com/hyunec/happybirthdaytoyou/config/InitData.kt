e9767aa78a3cd6eaa68eee99d90a53881f41fccc
Hyune-c <choi8608@gmail.com>
2022-11-23 23:37:20 +0900

    ✨ InitData 에서 memberPool 채우기

--- src/main/kotlin/com/hyunec/happybirthdaytoyou/config/InitData.kt
+++ src/main/kotlin/com/hyunec/happybirthdaytoyou/config/InitData.kt
@@ -1,24 +1,34 @@
 package com.hyunec.happybirthdaytoyou.config
 
 import com.hyunec.happybirthdaytoyou.domain.member.Member
+import com.hyunec.happybirthdaytoyou.domain.member.MemberPool
 import org.apache.commons.csv.CSVFormat
 import org.apache.commons.csv.CSVRecord
-import org.springframework.stereotype.Component
+import org.springframework.context.annotation.Configuration
 import java.io.FileReader
 import java.io.Reader
 import java.time.MonthDay
 import javax.annotation.PostConstruct
 
-@Component
-class InitData {
+@Configuration
+class InitData(private val memberPool: MemberPool) {
 
     @PostConstruct
     fun postConstruct() {
         val reader: Reader = FileReader(ClassLoader.getSystemResource("birthday.csv").file)
         val records: Iterable<CSVRecord> = CSVFormat.RFC4180.parse(reader)
-        for (record in records) {
-            val member = Member(record[0], MonthDay.parse(record[1]))
-            println("### $member, ${member.birthday.month}")
+        val newMembers = HashMap<MonthDay, MutableList<Member>>()
+
+        records.forEach { record ->
+            val name = record[0]
+            val birthDay = MonthDay.parse(record[1])
+
+            val members: MutableList<Member> = newMembers[birthDay] ?: mutableListOf()
+            members.add(Member(birthDay, name))
+
+            newMembers[birthDay] = members
         }
+
+        memberPool.reset(newMembers)
     }
 }
