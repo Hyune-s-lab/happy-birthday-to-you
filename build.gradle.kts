0c5ef3fbae87f0aa5dacf7fc0053ac6d37751464
Hyune-c <choi8608@gmail.com>
2022-11-24 00:00:19 +0900

    🔧 webflux dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,9 @@ val kotestExtVersion = "1.1.2"
 val fixtureMonkeyVersion = "0.4.5"
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
