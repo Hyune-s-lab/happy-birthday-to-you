a78ef70af972d8d6f338774747e0332d844b67aa
Hyune-c <choi8608@gmail.com>
2022-11-25 12:27:58 +0900

    ✨ SchedulerConfigure 설정

--- src/main/kotlin/com/hyunec/happybirthdaytoyou/config/SchedulerConfigure.kt
+++ src/main/kotlin/com/hyunec/happybirthdaytoyou/config/SchedulerConfigure.kt
@@ -1,6 +1,7 @@
 package com.hyunec.happybirthdaytoyou.config
 
 import com.hyunec.happybirthdaytoyou.domain.member.MemberPool
+import com.hyunec.happybirthdaytoyou.infra.slack.IncomingWebhookSender
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
@@ -8,16 +9,27 @@ import org.springframework.stereotype.Component
 import java.time.MonthDay
 
 @Component
-class SchedulerConfigure(private val memberPool: MemberPool) {
-
+class SchedulerConfigure(
+    private val slackWebhookPropertiesConfigure: SlackWebhookPropertiesConfigure,
+    private val incomingWebhookSender: IncomingWebhookSender,
+    private val memberPool: MemberPool,
+) {
     /**
      * 매 월 첫째날 9시
      */
     @Scheduled(cron = "0 0 9 1 * *")
+//    @Scheduled(cron = "* * * * * *") // 1초 단위로 실행
     fun everyFirstDayOfMonth() {
         val thisMonth = MonthDay.now().month
         val members = memberPool.find(thisMonth)
-        log.info("### $thisMonth=$members")
+        val slackWebhook = slackWebhookPropertiesConfigure.findWebhook("test")
+        members.forEach { m ->
+            val printString = slackWebhook.printString(mapOf("\$name" to m.name))
+            incomingWebhookSender.send(slackWebhook.key, printString)
+                .doOnSuccess { log.info("### $thisMonth=${m.name}") }
+                .doOnError { ex -> log.error("### ${ex.stackTrace}") }
+                .subscribe()
+        }
     }
 
     /**
@@ -27,7 +39,14 @@ class SchedulerConfigure(private val memberPool: MemberPool) {
     fun everyDay() {
         val today = MonthDay.now()
         val members = memberPool.find(today)
-        log.info("### $today=$members")
+        val slackWebhook = slackWebhookPropertiesConfigure.findWebhook("test")
+        members.forEach { m ->
+            val printString = slackWebhook.printString(mapOf("\$name" to m.name))
+            incomingWebhookSender.send(slackWebhook.key, printString)
+                .doOnSuccess { log.info("### $today=${m.name}") }
+                .doOnError { ex -> log.error("### ${ex.stackTrace}") }
+                .subscribe()
+        }
     }
 
     companion object {
