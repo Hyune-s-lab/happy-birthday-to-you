c5f6b338965aadeedd098f11111e8f04471e0b14
Hyune-c <choi8608@gmail.com>
2022-11-26 02:54:51 +0900

    ♻️ initData 파일 위치 변경

--- src/main/kotlin/com/hyunec/happybirthdaytoyou/config/InitData.kt
+++ src/main/kotlin/com/hyunec/happybirthdaytoyou/config/InitData.kt
@@ -2,6 +2,8 @@ package com.hyunec.happybirthdaytoyou.config
 
 import com.hyunec.happybirthdaytoyou.domain.member.MemberPool
 import com.hyunec.happybirthdaytoyou.infra.csv.CsvComponent
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
 import javax.annotation.PostConstruct
 
@@ -13,7 +15,13 @@ class InitData(
 
     @PostConstruct
     fun postConstruct() {
-        val newMembers = csvComponent.readMembers(ClassLoader.getSystemResource("birthday.csv").file)
+        val newMembers = csvComponent.readMembers("birthday.csv")
         memberPool.reset(newMembers)
+
+        log.info("### initData success. memberSize=${memberPool.find().size}")
+    }
+
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(this::class.java)
     }
 }
