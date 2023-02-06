00c0d3211899c845f6322e87193e28e2e388c969
Hyune-c <choi8608@gmail.com>
2023-02-06 15:35:51 +0900

    🔧 cors allow all

---
+++ src/main/kotlin/com/hyunec/happybirthdaytoyou/config/WebConfig.kt
@@ -0,0 +1,13 @@
+package com.hyunec.happybirthdaytoyou.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+    }
+}
